1615c61c4a463accf68f912e8d3cc3c0f1b767c5
Lucas Bibiano <lucasbibianodossantos@gmail.com>
2013-05-25 23:18:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1>Debacon-Rails</h1>
+<p>Base template for our rails applications.</p>
+<hr>
+<h2>Included gems</h2>
+<p>Debacon-Rails is built with the following gems:</p>
+<ul>
+  <li>Haml</li>
+  <li>Devise</li>
+  <li>Simple Form</li>
+  <li>Bourbon</li>
+  <li>Neat</li>
+</ul>
+<hr>
+<p>2013 © 4Soft</p>
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DebaconRails::Application.config.secret_token = '33df31941542d925927b1fcf913a87d42c61bb5ef5a2afdeba589027de3444faf4e0d1c1e0bc4a85c79aec0cedaec4c04126e219d24041e664ec73cb3e91d8c5'
